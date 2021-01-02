0a80470643d1468cf91d65d24eacbd4930a8f4e6
Andrew M McCall <andrew.m.mccall@gmail.com>
2021-01-01 22:07:40 -0500

    changed domain

---
+++ docs/js/script.js
@@ -0,0 +1,115 @@
+/**********
+ * Mobile Nav Toggle
+ */
+
+const before = document.querySelector(".hamburger::before");
+const after = document.querySelector(".hamburger::after");
+const hamburger = document.querySelector(".hamburger");
+const toggle = document.getElementById("nav-toggle");
+
+toggle.addEventListener("click", (e) => {
+  hamburger.classList.toggle("turn");
+});
+
+/**********
+ * Form validation
+ */
+
+let nameInput = document.getElementById("name");
+let emailInput = document.getElementById("email");
+let subjectInput = document.getElementById("subject");
+let messageInput = document.getElementById("message");
+const contactForm = document.getElementById("contact-form");
+let messages = [];
+
+function checkLength(input, callback) {
+  let message = input.value.trim();
+  if (!typeof message == String) {
+    return callback(input, "must be a string");
+  }
+  if (message === "") {
+    return callback(input, "cannot be empty");
+  }
+  if (message.length < 3) {
+    return callback(input, "length too short");
+  }
+  if (message.length > 14) {
+    return callback(input, "length too long");
+  }
+}
+
+function pushMessage(input, message) {
+  messages.push(message);
+  input.nextElementSibling.classList.add("fadeIn");
+  input.nextElementSibling.textContent = message;
+  setTimeout(() => {
+    input.nextElementSibling.classList.remove("fadeIn");
+  }, 3000);
+}
+
+function init() {
+  messages = [];
+  nameInput.value = "";
+  emailInput.value = "";
+  subjectInput.value = "";
+  messageInput.value = "";
+}
+
+function clearErrors() {
+  messages = [];
+  document.querySelectorAll("small").forEach((el) => {
+    el.classList.remove("fadeIn");
+  });
+}
+
+// Check email is valid
+
+function checkEmail(input, callback) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+  } else {
+    callback(input, "Email is not valid");
+  }
+}
+
+contactForm.addEventListener("submit", (e) => {
+  clearErrors();
+  checkLength(nameInput, pushMessage);
+  checkLength(emailInput, pushMessage);
+  checkLength(subject, pushMessage);
+  checkEmail(emailInput, pushMessage);
+  checkLength(messageInput, pushMessage);
+  console.log(messages);
+  if (messages.length > 0) {
+    e.preventDefault();
+  }
+});
+
+init();
+
+(function () {
+  const quotes = [
+    "men dressed in the color of summer speak like a dancer alone on the stage",
+    "we destroy ourselves with hope of futility and excrement",
+    "cockeyed lame goats drown moping exaltation strokes impotent shapeless",
+  ];
+  const slide = document.querySelector("#quote");
+
+  function quoteLoop() {
+    slide.innerText = quotes[quotes.length - 1]
+      let i = 0;
+      let updateQuote = setInterval(() => {
+        slide.classList.remove('fadeIn');
+        setTimeout(() => {
+          slide.innerText = quotes[i];
+        slide.classList.add('fadeIn');
+        i++;
+        if (i >= quotes.length) {
+          i = 0;
+        }
+        }, 1000)
+      }, 6000)
+  }
+
+  quoteLoop();
+})();
